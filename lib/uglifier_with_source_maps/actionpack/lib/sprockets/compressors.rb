9191750e3ffe80d570bd1915a64f4bee82d58271
Allan Bogh <ajbogh@allanbogh.com>
2016-10-26 12:59:51 -0700

    Update compressors.rb
    Added extend self to see if that would fix `undefined method 'register_js_compressor' for Sprockets::Compressors:Module` error.

--- lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
+++ lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
@@ -1,5 +1,7 @@
 module Sprockets
   module Compressors
+    extend self
+    
     register_js_compressor(:uglify_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
     register_js_compressor(:uglifier_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
   end
