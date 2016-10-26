45a0efb77e773fcf2a29ef47f5eb379c542213ce
Allan Bogh <ajbogh@allanbogh.com>
2016-10-26 13:10:59 -0700

    Update compressors.rb
    Commenting out previous code because it's causing a new error.

--- lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
+++ lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
@@ -2,10 +2,10 @@ module Sprockets
   module Compressors
     extend self
     
-    def register_js_compressor(name, klass, options = {})
-      @@default_js_compressor = name.to_sym if options[:default] || @@default_js_compressor.nil?
-      @@js_compressors[name.to_sym] = {:klass => klass.to_s, :require => options[:require]}
-    end
+#     def register_js_compressor(name, klass, options = {})
+#       @@default_js_compressor = name.to_sym if options[:default] || @@default_js_compressor.nil?
+#       @@js_compressors[name.to_sym] = {:klass => klass.to_s, :require => options[:require]}
+#     end
     
     register_js_compressor(:uglify_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
     register_js_compressor(:uglifier_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
