ac7c589bdc79d54af022ded952dc9f35feacbc0e
Allan Bogh <ajbogh@allanbogh.com>
2016-10-26 13:01:13 -0700

    Update compressors.rb
    Added definition for register_js_compressor to see if that would fix the error.

--- lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
+++ lib/uglifier_with_source_maps/actionpack/lib/sprockets/compressors.rb
@@ -4,6 +4,11 @@ module Sprockets
     
     register_js_compressor(:uglify_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
     register_js_compressor(:uglifier_with_source_maps, 'UglifierWithSourceMaps::Compressor', require: "uglifier_with_source_maps")
+    
+    def register_js_compressor(name, klass, options = {})
+      @@default_js_compressor = name.to_sym if options[:default] || @@default_js_compressor.nil?
+      @@js_compressors[name.to_sym] = {:klass => klass.to_s, :require => options[:require]}
+    end
   end
   class LazyCompressor #:nodoc:
     def compress(data, context = nil)
